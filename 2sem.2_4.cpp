8c9a71a6fb6cbc566b425259b88b8fc40bfc5f18
Maksimym200 <maksimym200@gmail.com>
2020-04-06 17:38:31 +0300

    2sem 2.Task_D

---
+++ 2sem.2_4.cpp
@@ -0,0 +1,307 @@
+﻿#include <iostream>
+#include <cassert>
+#include <stack>
+#include <queue>
+#include <vector>
+using std::stack;
+using std::pair;
+using std::vector;
+using std::queue;
+
+class graph
+{
+public:
+	graph() = default;
+	graph(int vertexNumber)
+	{
+		vNumber = vertexNumber;
+		eArray = vector<vector<int>>(vNumber);
+	}
+	void addEdge(int fVertex, int sVertex)
+	{
+		assert(fVertex < vNumber && sVertex < vNumber && fVertex >= 0 && sVertex >= 0);
+		eArray[fVertex].push_back(sVertex);
+	}
+	void addVertex()
+	{
+		eArray.push_back(vector<int>(0));
+	}
+	const vector<int>& next(int index) const
+	{
+		assert(index < vNumber && index >= 0);
+		return eArray[index];
+	}
+	int size() const
+	{
+		return vNumber;
+	}
+	void reverse()
+	{
+		vector<vector<int>> revEdges(vNumber);
+		for (int i = vNumber - 1; i >= 0; i--)
+		{
+			vector<int> edges = next(i);
+			for (int j = edges.size() - 1; j >= 0; j--)
+			{
+				revEdges[edges[j]].push_back(i);
+			}
+		}
+		*this = graph(revEdges);
+	}
+private:
+	graph(vector<vector<int>> edges)
+	{
+		eArray = edges;
+		vNumber = edges.size();
+	}
+	int vNumber = 0;
+	vector<vector<int>> eArray;
+};
+
+template <class TGraph, class TCollector>
+void BFS(TGraph& g, int startIndex, void (*visit)(int i, int d, TGraph& g, TCollector& c), TCollector& collector, bool* visited)
+{
+	queue<pair<int, int>> nextToVisit;
+	nextToVisit.push(pair<int, int>(startIndex, 0));
+	visited[startIndex] = true;
+	while (nextToVisit.size() != 0)
+	{
+		pair<int, int> vertex = nextToVisit.front();
+		nextToVisit.pop();
+		visit(vertex.first, vertex.second, g, collector);
+		const vector<int>& next = g.next(vertex.first);
+		int nextNumber = next.size();
+		for (int i = 0; i < nextNumber; i++)
+		{
+			if (!visited[next[i]])
+			{
+				nextToVisit.push(pair<int, int>(next[i], vertex.second));
+				visited[next[i]] = true;
+			}
+		}
+	}
+}
+
+template <class TGraph, class TCollector>
+void BFS(TGraph& g, int startIndex, void (*visit)(int i, int d, TGraph& g, TCollector& c), TCollector& collector)
+{
+	bool* visited = new bool[g.size()];
+	for (int i = g.size() - 1; i >= 0; i--)
+	{
+		visited[i] = false;
+	}
+	BFS(g, startIndex, visit, collector, visited);
+	delete[] visited;
+}
+
+bool topSort(const graph& g, vector<int>& arr)
+{
+	bool result = true;
+	arr.clear();
+	int size = g.size();
+	int* colors = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		colors[i] = 0;
+	}
+	stack<int> toVisit;
+	for (int i = 0; i < size; )
+	{
+		if (colors[i] == 2)
+		{
+			i++;
+			continue;
+		}
+		toVisit.push(i);
+		while (toVisit.size() != 0)
+		{
+			int v = toVisit.top();
+			toVisit.pop();
+			if (colors[v] == 1)
+			{
+				colors[v] = 2;
+				arr.push_back(v);
+			}
+			if (colors[v] == 0)
+			{
+				colors[v] = 1;
+				toVisit.push(v);
+				vector<int> next = g.next(v);
+				for (int j = next.size() - 1; j >= 0; j--)
+				{
+					if (colors[next[j]] == 1)
+					{
+						result = false;
+					}
+					else
+					{
+						toVisit.push(next[j]);
+					}
+				}
+			}
+		}
+	}
+	delete[] colors;
+	return result;
+}
+
+void collectVertex(int index, int depth, const graph& g, vector<int>& arr)
+{
+	arr.push_back(index);
+}
+
+void findSCC(const graph& g, vector<vector<int>>& SCCarr)
+{
+	SCCarr.clear();
+	bool* visited = new bool[g.size()];
+	for (int i = g.size() - 1; i >= 0; i--)
+	{
+		visited[i] = false;
+	}
+	graph gRev = g;
+	gRev.reverse();
+	vector<int> arr;
+	topSort(gRev, arr);
+	for (int i = arr.size() - 1; i >= 0; i--)
+	{
+		if (visited[arr[i]])
+		{
+			continue;
+		}
+		vector<int> SCC;
+		SCCarr.push_back(SCC);
+		BFS<const graph, vector<int>>(g, arr[i], collectVertex, SCCarr[SCCarr.size() - 1], visited);
+	}
+	delete[] visited;
+}
+
+void getSCCGraph(const graph& g, graph& SCCGraph, const vector<vector<int>>& SCCarr)
+{
+	vector<int> SCCBelonging(g.size());
+	for (int i = SCCarr.size() - 1; i >= 0; i--)
+	{
+		for (int j = SCCarr[i].size() - 1; j >= 0; j--)
+		{
+			SCCBelonging[SCCarr[i][j]] = i;
+		}
+	}
+	SCCGraph = graph(SCCarr.size());
+	for (int i = SCCarr.size() - 1; i >= 0; i--)
+	{
+		vector<bool> edges(SCCarr.size(), false);
+		for (int j = SCCarr[i].size() - 1; j >= 0; j--)
+		{
+			vector<int> neighbours = g.next(SCCarr[i][j]);
+			for (int k = neighbours.size() - 1; k >= 0; k--)
+			{
+				if (!edges[SCCBelonging[neighbours[k]]])
+				{
+					SCCGraph.addEdge(i, SCCBelonging[neighbours[k]]);
+				}
+				edges[SCCBelonging[neighbours[k]]] = true;
+			}
+		}
+	}
+}
+
+template <class TVisit, class TCollector>
+void edgeBFS(const graph& g, int startIndex, const TVisit& visit, TCollector& collector)
+{
+	bool* visited = new bool[g.size()];
+	for (int i = g.size() - 1; i >= 0; i--)
+	{
+		visited[i] = false;
+	}
+	queue<int> nextToVisit;
+	nextToVisit.push(startIndex);
+	visited[startIndex] = true;
+	while (nextToVisit.size() != 0)
+	{
+		int vertex = nextToVisit.front();
+		nextToVisit.pop();
+		const vector<int>& next = g.next(vertex);
+		int nextSize = next.size();
+		for (int i = 0; i < nextSize; i++)
+		{
+			visit(next[i], g, collector);
+			if (!visited[next[i]])
+			{
+				nextToVisit.push(next[i]);
+				visited[next[i]] = true;
+			}
+		}
+	}
+	delete[] visited;
+}
+
+void visit(int index, const graph& g, vector<bool>& arr)
+{
+	arr[index] = true;
+}
+
+void getTransitiveClosure(const graph& g, vector<vector<bool>>& closure)
+{
+	closure.clear();
+	closure = vector<vector<bool>>(g.size(), vector<bool>(g.size(), false));
+	for (int i = 0; i < g.size(); i++)
+	{
+		edgeBFS(g, i, visit, closure[i]);
+	}
+}
+
+void purdom(const graph& g, vector<vector<bool>>& closure)
+{
+	vector<vector<int>> SCCarr;
+	findSCC(g, SCCarr);
+	graph SCCGraph;
+	getSCCGraph(g, SCCGraph, SCCarr);
+	closure.clear();
+	closure = vector<vector<bool>>(g.size(), vector<bool>(g.size(), false));
+	vector<vector<bool>> SCCClosure;
+	getTransitiveClosure(SCCGraph, SCCClosure);
+	for (int i = 0; i < SCCarr.size(); i++)
+	{
+		for (int j = 0; j < SCCarr.size(); j++)
+		{
+			if (SCCClosure[i][j])
+			{
+				for (int k = SCCarr[i].size() - 1; k >= 0; k--)
+				{
+					for (int t = SCCarr[j].size() - 1; t >= 0; t--)
+					{
+						closure[SCCarr[i][k]][SCCarr[j][t]] = true;
+					}
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	int N;
+	std::cin >> N;
+	graph g(N);
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			char edge;
+			std::cin >> edge;
+			if (edge - '0')
+			{
+				g.addEdge(i, j);
+			}
+		}
+	}
+	vector<vector<bool>> ways;
+	purdom(g, ways);
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			std::cout << (ways[i][j]);
+		}
+		std::cout << "\n";
+	}
+}
